bb3f374e5b231d427ffdda90a579283326f67ca4
ESDarkLink <42819919+ESDarkLink@users.noreply.github.com>
2023-01-04 10:03:18 -0600

    v3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyreactgraphqle184ef05": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import './App.css';
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <View className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>We now have Auth!</h1>
+          <Card>
+            <Button className='Button' onClick={signOut}>Sign Out</Button>
+          </Card>
+        </header>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
